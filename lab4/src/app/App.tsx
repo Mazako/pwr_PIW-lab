3bc99b38b0e182ef4afc370adcf0a7ce9829f01b
michał <michal.maziarz12@gmail.com>
2024-05-01 01:10:49 +0200

    Basic google redirect login

--- lab4/src/app/App.tsx
+++ lab4/src/app/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import '../dialog-base.css';
 import {createBrowserRouter, createRoutesFromElements, Route} from "react-router-dom";
 import {Layout} from "../pages/layout/Layout";
@@ -8,8 +8,15 @@ import {FavoriteOffersPage} from "../pages/favorite-offers/FavoriteOffersPage";
 import {MyOffersPage} from "../pages/my-offers/MyOffersPage";
 import {MainHotelPage} from "../pages/hotel-page/MainHotelPage";
 import {HeroPage} from "../pages/hero-page/HeroPage";
-
+import {getRedirectResult} from 'firebase/auth'
+import {auth} from "../firebase/firebase";
 const App: React.FC = () => {
+
+    useEffect(() => {
+        getRedirectResult(auth)
+            .then(r => console.log(r))
+    }, [])
+
     const router = createBrowserRouter(createRoutesFromElements([
         <Route path='/' element={<Layout/>}>
             <Route index element={<HeroPage />} />
--- lab4/src/components/navbar/NavBar.tsx
+++ lab4/src/components/navbar/NavBar.tsx
@@ -2,6 +2,7 @@ import styles from './NavBar.module.css'
 import React, {FC, useRef} from "react";
 import {NavLink} from "react-router-dom";
 import {AddModal} from "../add-modal/AddModal";
+import {googleRedirectLogin} from "../../firebase/firebase";
 
 export const Navbar: FC = () => {
     const ref = useRef<HTMLDialogElement>(null);
@@ -14,6 +15,14 @@ export const Navbar: FC = () => {
         );
     }
 
+    const handleLogin = async () => {
+        try {
+             await googleRedirectLogin();
+        } catch (e) {
+            console.error('login error')
+        }
+    }
+
     return (
         <nav className={styles.navbar}>
             <img src="/assets/icons/logo.svg" className={styles.logo} alt="logo"/>
@@ -24,7 +33,7 @@ export const Navbar: FC = () => {
                 <li><a href="#" onClick={() =>  ref.current?.showModal()}>Add new offers</a></li>
                 <li>{navLinkFactory('/my-offers', 'My offers')}</li>
                 <li>{navLinkFactory('/favorite', 'Favorites')}</li>
-                <button className="button primary">Log in</button>
+                <button className="button primary" onClick={handleLogin}>Log in</button>
             </ul>
             <button className="button primary hidden">Menu</button>
             <AddModal ref={ref}/>
